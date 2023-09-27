d25c0805c5b5de943fdc9348494892b195e124cd
Franklyn Aquino <f.aquino@lukka.global>
2023-09-27 10:53:48 -0400

    updated vertica query & opensearch api search

--- src/run.py
+++ src/run.py
@@ -16,20 +16,34 @@ try:
     
     pcur = pcon.cursor()
     logger.info(f'Starting the query....')
-    query = '''
-            select j.job_id
-            from lco.jobs j
-            left join account_service_sync.lco_accounts la on j.account_id = la.account_id 
-            left join account_service_sync.lco_data_providers ldp on la.provider_id = ldp.provider_id 
-            left join account_service_sync.lco_entities le on j.entity_id = le.entity_id
-            left join account_service_sync.lco_users u on u.user_id = j.user_id
-            where j.error_message ilike CONCAT('%',CONCAT('stale','%'))
-            and date(last_updated_ts) > now()::date - 7
-            order by last_updated_ts desc
-            limit 50'''
+    query = ('''
+                                    select j.job_id
+                                    , j.entity_id
+                                    , j.job_status
+                                    , j.job_type
+                                    ,COALESCE(j.connector_transaction_type,j.connector_blockchain_transaction_type,'N/A') transaction_type
+                                    , j.last_updated_ts
+                                    , (now() - j.start_ts) as duration
+                                    from lco.v_all_jobs j
+                                    left join account_service_sync.lco_accounts la on j.account_id = la.account_id 
+                                    left join account_service_sync.lco_data_providers ldp on la.provider_id = ldp.provider_id
+                                    left join account_service_sync.lco_entities le on j.entity_id = le.entity_id
+                                    left join account_service_sync.lco_users u on u.user_id = j.user_id
+                                    where j.job_status not in ('COMPLETED', 'FAILED','DELETED')
+                                    and j.job_type in ('CONNECTOR_API','CONNECTOR_BLOCKCHAIN_API')
+                                    and duration > '02:00:00'
+                                    and j.account_id not in ('3fe2191d-ce15-4bb0-8d03-9136f38c2939',
+                                    'dd696098-f7de-4277-8b14-303ea5a29036',
+                                    'f33b1c0a-0bc2-44b9-a067-1f7817f1fee2',
+                                    'cddc46de-9063-4bc8-bbbe-99a37b7071e0',
+                                    'f33b1c0a-0bc2-44b9-a067-1f7817f1fee2') 
+                                    and date(last_updated_ts) >= now()::date - 1
+                                    order by j.last_updated_ts desc
+                                    limit 200''')
     response = pcur.execute(query)
     
     list_of_opensearch_objects = []
+    list_of_zombie_jobs:List[VerticaObject] = []
     
     #Call Kibana/opensearch one at a time.
     for row in response.iterate():
@@ -48,7 +62,10 @@ try:
         search_response:dict = opensearch.run_search(size=1)
         # logger.info(search_response)
         
-        hits = search_response['hits']['hits']
+        try:
+            hits = search_response['hits']['hits']
+        except KeyError as e:
+            list_of_zombie_jobs.append(v_object.job_id)
         
         for hit in hits:
             if any(x for x in hit['_source']):
@@ -62,11 +79,13 @@ try:
                     logger.info(f'KeyError found -> {e}')
                 finally:
                     opensearch_response_list.append(opensearch_response)
+            else:
+                list_of_zombie_jobs.append(v_object.job_id)
         
     # joined_response = ','.join(opensearch_response_list)
     
-    for response in opensearch_response_list:
-        logger.info(f'Joined response: {response.to_string()}')
+    for response in list_of_zombie_jobs:
+        logger.info(f'Joined response: {response.job_id}')
         
 except error.ConnectionError as e:
     logger.error(e)
